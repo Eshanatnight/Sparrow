934e9e1c632b74d356f9573299e05b3eb5953756
Eshan Chatterjee <eshan2001221@gmail.com>
2022-05-26 14:56:45 +0530

    Moved to a testing folder

--- test/test.ps1
+++ test/test.ps1
@@ -1,14 +1,14 @@
 # Build Sparrow
-.\.script\build_sparrow.ps1;
+..\.scripts\build_sparrow.ps1;
 
 # Creates the Folders for the Tests, so it's neater and more organized for me
-.\.script\test_paths.ps1;
+..\.scripts\test_paths.ps1;
 
 # Tests
-.\.script\test_compilation.ps1;
+..\.scripts\test_compilation.ps1;
 
 # Move the files to their respective folders
-.\.script\move.ps1;
+..\.scripts\move.ps1;
 
 # Test Executables
-.\.script\test_executables.ps1;
+..\.scripts\test_executables.ps1;
